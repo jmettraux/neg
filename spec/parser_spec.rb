b33135f4dc22367f4da45e0ab81a5f44c9f80fdc
John Mettraux <jmettraux@gmail.com>
2012-09-10 19:50:59 +0900

    reorg spec a bit

---
+++ spec/parser_spec.rb
@@ -0,0 +1,9 @@
+
+require 'spec_helper'
+
+
+describe Leg::Parser do
+
+  it 'flips burgers'
+end
+
--- spec/string_parser_spec.rb
+++ spec/string_parser_spec.rb
@@ -2,17 +2,27 @@
 require 'spec_helper'
 
 
-describe '`x`' do
+describe Leg::Parser::StringParser do
 
   it 'parses an exact string' do
 
-    class Parser < Leg::Parser
-      def root
-        `x`
-      end
-    end
+    parser = Leg::Parser::StringParser.new('xxx')
 
-    Parser.new.parse('x').should == true
+    parser.parse('xxx').should == true
+  end
+
+  context 'within Leg::Parser' do
+
+    it 'parses an exact string' do
+
+      class Parser < Leg::Parser
+        def root
+          `x`
+        end
+      end
+
+      Parser.new.parse('x').should == true
+    end
   end
 end
 
