eeab7e538a3416ad2277a93424532a3eb4f1001e
John Mettraux <jmettraux@gmail.com>
2012-10-07 14:35:01 +0900

    add arith exp sample

---
+++ spec/sample_arith_spec.rb
@@ -0,0 +1,56 @@
+
+require 'spec_helper'
+
+
+describe 'sample math parser' do
+
+  class ArithParser < Neg::Parser
+
+    expression  == operation
+
+    operation   == value + ((`+` | `-` | `*` | `/`) + value) * 0
+    value       == parenthese | number
+    parenthese  == `(` + expression + `)`
+    number      == `-` * -1 + _('0-9') * 1
+  end
+
+  def parse(s)
+
+    r = ArithParser.parse(s)
+
+    if ENV['DEBUG'] == 'true' # /!\ not $DEBUG
+      puts "--#{s.inspect}-->"
+      pp r
+    end
+
+    r[2]
+  end
+
+  it 'parses numbers' do
+
+    parse("0").should == true
+    parse("-0").should == true
+    parse("12").should == true
+    parse("-3").should == true
+    parse("-12").should == true
+  end
+
+  it 'parses parentheses' do
+
+    parse("(1)").should == true
+    parse("((1))").should == true
+  end
+
+  it 'parses operations' do
+
+    parse("1+1").should == true
+    parse("1+-1").should == true
+  end
+
+  it 'parses at large' do
+
+    parse("1+(1+1)").should == true
+    parse("12+(34-(56/78))").should == true
+  end
+end
+
